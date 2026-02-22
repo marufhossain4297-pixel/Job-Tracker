393d9207fcaaab6de14a3e6d94dab0bd55b7f40b
Tahmina Haque <tahminahaque@Tahminas-MacBook-Air.local>
2026-02-22 14:25:36 +0100

    add javascript

--- script.js
+++ script.js
@@ -20,9 +20,6 @@ console.log(allCardSection.children.length)
 function calculateCount() {
     total.innerText = allCardSection.children.length;
     availableJobs.innerText = allCardSection.children.length;
-    
-    interview.innerText = interviewList.length
-    rejected.innerText = rejectedList.length
 }
 calculateCount()
 
